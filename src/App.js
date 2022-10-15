ced598abb7e661a66506c42b81039bb2911446dc
Rakta1 <Rakta0613@gmail.com>
2022-10-15 10:57:40 -0400

    included github source

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import "./App.css";
 
 export default function App() {
-	return <div className="App">Dictionary Project</div>;
+	return (
+		<div className="App">
+			Dictionary Project
+			<footer>
+				<a href="https://github.com/Rakta1/Dictionary-project">Open-Source</a>,
+				coded by Terra Wardana
+			</footer>
+		</div>
+	);
 }
