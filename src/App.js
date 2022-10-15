95d8988d8453740b9bdf68d5d262ba88a9e597f5
Rakta1 <Rakta0613@gmail.com>
2022-10-15 11:46:18 -0400

    dictionary component and events

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 export default function App() {
 	return (
 		<div className="App">
-			Dictionary Project
-			<footer>
-				<a href="https://github.com/Rakta1/Dictionary-project">Open-Source</a>,
-				coded by Terra Wardana
-			</footer>
+			<div className="container">
+				Dictionary Project
+				<Dictionary />
+				<footer>
+					<a href="https://github.com/Rakta1/Dictionary-project">Open-Source</a>
+					, coded by Terra Wardana
+				</footer>
+			</div>
 		</div>
 	);
 }
